d9c6837c25bad6b4687ee7ade654988c8a01f555
ShaheerAlisha <85605738+ShaheerAlisha@users.noreply.github.com>
2021-06-15 21:22:37 +0530

    update runner

--- PremierInnWebAppPortal/src/main/java/com/qa/runner/TestRunner.java
+++ PremierInnWebAppPortal/src/main/java/com/qa/runner/TestRunner.java
@@ -6,11 +6,8 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "html:target/cucumberHtmlReport/cucumber-html-report.html",
-        "json:target/cucumberHtmlReport/cucumber-json-report.json",
-        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}, glue = {
-        "com.qa.stepDefinition"}, features = "classpath:Features"
-
+@CucumberOptions(plugin = {"pretty","html:target/cucumberHtmlReport/cucumber-html-report.html"}, glue = {
+        "com.qa.stepDefinition"}, features = "classpath:features"
 )
 public class TestRunner {
 }
